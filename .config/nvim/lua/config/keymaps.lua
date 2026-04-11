dfc86d3e3d688e2fad34a87ee709a03581f8eadd
Edison Chong <kurai.no.yozora@gmail.com>
2026-04-11 11:27:36 +0800

    feat: nvim keymap <c-o> opencode

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,21 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+-- opencode
+if vim.fn.executable("opencode") == 1 then
+  map({ "n", "t" }, "<c-o>", function()
+    Snacks.terminal.focus("opencode", {
+      interactive = true,
+      win = {
+        position = "float",
+        backdrop = 60,
+        height = 0.9,
+        width = 0.95,
+        zindex = 50,
+      },
+    })
+  end, { desc = "OpenCode" })
+end

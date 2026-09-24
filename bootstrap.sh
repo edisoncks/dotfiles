86e0459da17114f9d972722a8a0796fd12815dc4
Edison Chong <kurai.no.yozora@gmail.com>
2026-09-24 22:01:13 +0800

    feat(delta): use catppuccin mocha theme

--- bootstrap.sh
+++ bootstrap.sh
@@ -22,6 +22,7 @@ fi
 DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 declare -a DOTFILES=(
   ".bashrc.d"
+  ".config/delta"
   ".config/ghostty"
   ".config/lazygit"
   ".config/mise"
